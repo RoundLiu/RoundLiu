5c0527bfea80b87eb1baa95948f8e19e22a5c0dd
Roun <1710707653@qq.com>
2016-09-22 11:30:49 +0800

    2016.9.22  星期四  新建项目

---
+++ app/src/main/java/com/tianruan/round/GotoActivity.java
@@ -0,0 +1,13 @@
+package com.tianruan.round;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.one);
+    }
+}
