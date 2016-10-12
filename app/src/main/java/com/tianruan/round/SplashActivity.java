65eaeb677aba751d0c62bad2dd8ed3f70aeb4a15
Roun <1710707653@qq.com>
2016-10-12 10:54:47 +0800

    2016年10月11日代码

--- app/src/main/java/com/tianruan/round/SplashActivity.java
+++ app/src/main/java/com/tianruan/round/SplashActivity.java
@@ -2,16 +2,21 @@ package com.tianruan.round;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.Window;
-
+import android.widget.ImageView;
 import com.tianruan.round.Util.PrefUtils;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class SplashActivity extends Activity {
+    //显示图片
+    ImageView imageView;
 
+    AnimationDrawable myanimation;//声明AnimationDrawable类（可以自定义名字）
+    Resources res;//声明resources资源
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +25,22 @@ public class SplashActivity extends Activity {
         startSplash();
        // gotoNextPage();
     }
+
+    private void setAnimationDrable(){
+        imageView=(ImageView)findViewById(R.id.imageView);
+        imageView.setBackgroundResource(R.drawable.animationdrable);
+        myanimation=(AnimationDrawable)imageView.getBackground();
+        myanimation.start();
+    }
+
+
+
     private  void startSplash(){
-        //5秒的延迟
+
+        setAnimationDrable();
+
+
+        //1.  5秒的延迟
         //初始化计时器
         Timer timer=new Timer();
         //初始化计时器任务
@@ -30,11 +49,12 @@ public class SplashActivity extends Activity {
             public void run() {
 
                 //startActivity(new Intent(SplashActivity.this,GuideActivity.class));
+                myanimation.stop();
                 gotoNextPage();
             }
         };
         //启动定时器
-        timer.schedule(task,2000);
+        timer.schedule(task,4000);
     }
 
     //闪屏跳转到下一个界面
